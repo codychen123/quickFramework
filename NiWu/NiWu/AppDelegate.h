2ad2167f4b37979d705fc1b2262987847b16839e
ithuihui <chhhithuihui@gmail.com>
2017-05-05 15:08:47 +0800

    导入pod

---
+++ NiWu/NiWu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NiWu
+//
+//  Created by xiaohuihui on 2017/5/5.
+//  Copyright © 2017年 MeiZhiBangCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
